9fd5f54bee7e0073643ff47dec571ce6a1312297
KillReal2287 <89987040+KillReal2287@users.noreply.github.com>
2021-11-15 18:33:45 +0700

    Calculator

--- task_1_4_1/src/main/java/Solution.java
+++ task_1_4_1/src/main/java/Solution.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello world");
+
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
         Calculator subject = new Calculator();
