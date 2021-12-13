077de4480b73097e625da38fd5af68af4f1d1d32
KillReal2287 <89987040+KillReal2287@users.noreply.github.com>
2021-12-13 17:19:15 +0700

    added

---
+++ Final/src/main/java/Note.java
@@ -0,0 +1,23 @@
+import java.util.Date;
+
+public class Note{
+    private final String name;
+    private final String note;
+    private final Date time;
+
+    Note(String title, String note, Date time){
+        this.name = title;
+        this.note = note;
+        this.time = time;
+    }
+
+    public Date getTime(){
+        return time;
+    }
+    public String getNote(){
+        return note;
+    }
+    public String getTitle(){
+        return name;
+    }
+}
---
+++ Final/src/main/java/Notebook.java
@@ -0,0 +1,78 @@
+import com.google.gson.Gson;
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.Date;
+public class Notebook {
+    private final String filename;
+    private final static Gson gson = new Gson();
+    public Notebook(){
+        this.filename = "notes.json";
+    }
+    public Notebook(String name) {
+        this.filename = name;
+    }
+    public List<Note> take(){
+        try (Reader reader = new FileReader(filename)){
+            Note[] notes = gson.fromJson(reader, Note[].class);
+            return new ArrayList<>(Arrays.asList(notes));
+        }
+        catch (Exception e){
+            return new ArrayList<>();
+        }
+    }
+    public void write(List<Note> notes){
+        try (Writer writer = new FileWriter(filename)){
+            gson.toJson(notes, writer);
+        }
+        catch (IOException e){
+            System.err.println("Failed to save notes" + e.getMessage());
+        }
+    }
+    public void add(Note note){
+        List<Note> notes = take();
+        notes.add(note);
+        write(notes);
+    }
+    public void remove(String title){
+        List<Note> notes = take();
+        if (notes.removeIf(note -> note.getTitle().equals(title)))
+            write(notes);
+        else
+            System.err.println("Failed to delete note");
+    }
+    public void writeNotes(Note[] notes){
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        for (Note note : notes){
+            System.out.println(note.getTitle());
+            System.out.println(note.getNote());
+            System.out.println(dateFormat.format(note.getTime()));
+            System.out.println();
+        }
+    }
+    public void show(){
+        Note[] notes = take().toArray(new Note[0]);
+        writeNotes(notes);
+    }
+    public void show(Date startTime, Date endTime, String[] words){
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        List<Note> notes = take();
+        Note[] notes = (Note[]) notes.stream().filter(note -> note.getTime().after(startTime) && note.getTime().before(endTime)).toArray();
+        int flag;
+        for (Note note: notes) {
+            System.out.println(note.getTitle());
+            System.out.println(note.getNote());
+            System.out.println(dateFormat.format(note.getTime()));
+            System.out.println();
+
+        }
+    }
+    public void deleteNotebook(){
+        File file = new File(filename);
+        if (file.delete())
+            System.out.println("Deleted the file: " + file.getName());
+        else
+            System.out.println("Failed to delete the file.");
+    }
+}
